3a3a18dbb972112ad6af9090c3bf8aeac86e50e8
Ruth <rromerocarretero.cf@iesesteveterradas.cat>
2026-01-26 19:20:51 +0100

    funciona el login

---
+++ admin/index.php
@@ -0,0 +1,24 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles.css">
+    <title>Admin - SIMBIO</title>
+</head>
+<body id=login-body>
+    <header class="main-header">
+        <h1 class="header-title">SIMBIO</h1>
+    </header>
+    <section class="admin">
+        <h2>Benvingut/da, administrador/a!</h2>
+        <p>Utilitza el menú de navegació per gestionar el sistema.</p>
+        <ul>
+            <li><a href="admin/projectes.php">Gestionar projectes</a></li>
+        </ul>
+    </section>
+</body>
+</html>
