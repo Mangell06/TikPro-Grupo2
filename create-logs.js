bde847bf153fd02c489634825f78854f560247e1
Miguel Ángel López <mangell0624@gmail.com>
2026-01-28 16:50:48 +0100

    change directory logs

--- create-logs.js
+++ create-logs.js
@@ -2,27 +2,27 @@
 let cachedIp = null;
 
 async function getClientIp() {
-    if (cachedIp) return cachedIp;
+  if (cachedIp) return cachedIp;
 
-    try {
-        const res = await fetch('https://api.ipify.org?format=json');
-        const data = await res.json();
-        cachedIp = data.ip;
-        return cachedIp;
-    } catch (e) {
-        return 'IP_JS_desconocida';
-    }
+  try {
+    const res = await fetch("https://api.ipify.org?format=json");
+    const data = await res.json();
+    cachedIp = data.ip;
+    return cachedIp;
+  } catch (e) {
+    return "IP_JS_desconocida";
+  }
 }
 
 export async function sendLog(text) {
-    const ip = await getClientIp();
+  const ip = await getClientIp();
 
-    fetch('logs/logs.php', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            text,
-            client_ip: ip
-        })
-    }).catch(err => console.error('Error log:', err));
+  fetch("includes/logs.php", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      text,
+      client_ip: ip,
+    }),
+  }).catch((err) => console.error("Error log:", err));
 }
